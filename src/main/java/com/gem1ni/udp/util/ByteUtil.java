9b4178be52f0de42ff214e700880f4a1dc85b581
Gem1ni <gem1ni.sky31@foxmail.com>
2016-09-23 02:03:01 +0800

    update copyright

--- src/main/java/com/gem1ni/udp/util/ByteUtil.java
+++ src/main/java/com/gem1ni/udp/util/ByteUtil.java
@@ -1,17 +1,21 @@
-/*
- * Copyright 2016 Gem1ni
+/* This software was developed by employees of the National Institute of
+ * Standards and Technology (NIST), an agency of the Federal Government.
+ * Pursuant to title 15 United States Code Section 105, works of NIST
+ * employees are not subject to copyright protection in the United States
+ * and are considered to be in the public domain.  As a result, a formal
+ * license is not needed to use the software.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * This software is provided by NIST as a service and is expressly
+ * provided "AS IS".  NIST MAKES NO WARRANTY OF ANY KIND, EXPRESS, IMPLIED
+ * OR STATUTORY, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTY OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, NON-INFRINGEMENT
+ * AND DATA ACCURACY.  NIST does not warrant or make any representations
+ * regarding the use of the software or the results thereof including, but
+ * not limited to, the correctness, accuracy, reliability or usefulness of
+ * the software.
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Permission to use this software is contingent upon your acceptance
+ * of the terms of this agreement.
  */
 package com.gem1ni.udp.util;
 
