b21d19a767e0e9ea26351ca0b25d16fcaf9fca65
Gem1ni <gem1ni.sky31@foxmail.com>
2016-09-23 01:58:44 +0800

    update copyright

--- src/main/java/com/gem1ni/udp/impl/IReceiver.java
+++ src/main/java/com/gem1ni/udp/impl/IReceiver.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 Gem1ni
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.gem1ni.udp.impl;
 
 import java.net.InetAddress;
@@ -8,17 +24,17 @@ import java.net.InetAddress;
  */
 public interface IReceiver extends IBase {
 
-    interface OnReceiveListener {
-
-        void onReceiveReply(byte[] bytes, int offset, int length, InetAddress address, int port);
-
-        void onReceiveFailed(Exception e);
-    }
-
     void receive();
 
     void shutdown();
 
     @Override
     void run();
+
+    interface OnReceiveListener {
+
+        void onReceiveReply(byte[] bytes, int offset, int length, InetAddress address, int port);
+
+        void onReceiveFailed(Exception e);
+    }
 }
